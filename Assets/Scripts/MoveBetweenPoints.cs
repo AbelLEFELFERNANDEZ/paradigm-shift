af99a3c14ccfa6f2e677c36baac96ebb90b76999
AlderGodric <pixelhunter39@gmail.com>
2020-11-25 12:03:42 +0100

    Unity version change, Saw

---
+++ Assets/Scripts/MoveBetweenPoints.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class MoveBetweenPoints : MonoBehaviour
+{
+
+    public Transform pointA;
+    public Transform pointB;
+    public float speed = 2;
+
+    private bool aToB = true;
+
+    private void Start()
+    {
+        transform.position = pointA.position;
+    }
+
+    private void Update()
+    {
+        if (aToB)
+        {
+            if(transform.position != pointB.position)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, pointB.position, speed * Time.deltaTime);
+            } else
+            {
+                aToB = false;
+            }
+        } else
+        {
+            if (transform.position != pointA.position)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, pointA.position, speed * Time.deltaTime);
+            }
+            else
+            {
+                aToB = true;
+            }
+        }
+        
+    }
+
+
+}

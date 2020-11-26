cdd0c63a2adc5576a1c287fc0c313f4f264c6243
AcxsaFTG <theo.guillonneau@gmail.com>
2020-11-26 23:46:32 +0100

    Door + MusicMenu

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
+    AudioSource Audio;
+
+    private void Start()
+    {
+        Audio = GetComponent<AudioSource>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.GetComponent<PlayerController>() && GameController.instance.hasKey)
         {
+            Audio.Play();
             transform.Find("Lock").gameObject.SetActive(false);
             GetComponent<Animator>().Play("DoorOpen");
             GameController.instance.Win();

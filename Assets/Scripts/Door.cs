777552d9f8994cd78c0f8c24dc6482c70a3e9c8e
AcxsaFTG <theo.guillonneau@gmail.com>
2020-11-27 12:32:12 +0100

    MenuSound

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -4,18 +4,20 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
-    AudioSource Audio;
+     public AudioSource Opening;
+     public AudioSource Win;
 
     private void Start()
     {
-        Audio = GetComponent<AudioSource>();
+        //Audio = GetComponent<AudioSource>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.GetComponent<PlayerController>() && GameController.instance.hasKey)
         {
-            Audio.Play();
+            Win.Play();
+            Opening.Play();
             transform.Find("Lock").gameObject.SetActive(false);
             GetComponent<Animator>().Play("DoorOpen");
             GameController.instance.Win();
